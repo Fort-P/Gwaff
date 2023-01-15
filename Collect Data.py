f9d0381a54ac908089378b3af5ac511cdbaa72c7
Fort-P <74835871+Fort-P@users.noreply.github.com>
2023-01-14 23:08:45 -0500

    Added Main Files

---
+++ Collect Data.py
@@ -0,0 +1,120 @@
+'''
+grab data
+    dynamically create a link to the api endpoint
+    send http request to get data
+    return data for further handling
+send data to external database (sql)
+    save columns
+        username
+        nickname
+        highest role color
+        icon
+        date/time (this will contain xp at given date/time)
+    save and close database
+'''
+
+# Import Libraries
+import json
+import requests
+from time import sleep
+from datetime import datetime, timedelta
+from sqlalchemy import create_engine, text
+
+# Declare Global Vars
+base_url = 'https://gdcolon.com/polaris/api/leaderboard/377946908783673344'
+table = 'Gwaff'
+time = datetime.now()
+hour_later = time + timedelta(hours=1)
+
+# Grab Credentials (you don't think I would be stupid enough to store them within this file)
+with open('credentials.json') as file:
+    credentials = json.load(file)
+
+username = credentials['username']
+password = credentials['password']
+host = credentials['host']
+dbname = credentials['dbname']
+
+# Initiallize SQL
+engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}/{dbname}')
+
+# Define functions
+def get_data(url, page):
+    '''
+    queries an api endpoint at {url+page}, and returns that data as a python dictionary object
+    '''
+    url += f'?page={page}'
+    print(f"Sending HTTP GET request to {url}... ", end="")
+    response = requests.get(url)
+    print("Status code:", response.status_code)
+    data = response.json()
+    return data
+
+def record_data(url, pages, table, time):
+    '''
+    Records "pages" pages of data, and saves them to a database specified in G{engine}, in table G{table}
+    '''
+    id = 0
+    username = ""
+    nickname = ""
+    color = ""
+    icon = ""
+    xp = 0
+    rank = 0
+    
+    with engine.connect() as con:
+        try:
+#             con.execute(f"ALTER TABLE {table} ADD COLUMN {time} INT NULL DEFAULT NULL")
+            pass
+        except:
+            pass
+    
+    for i in pages:
+        data = get_data(url, i)
+        leaderboard = data['leaderboard']
+        for t in range(0, len(leaderboard)):
+            try:
+                username = leaderboard[t]['username']
+                if leaderboard[t]['nickname'] == None:
+                    nickname = leaderboard[t]['username']
+                else:
+                    nickname = leaderboard[t]['nickname']
+                color = leaderboard[t]['color']
+                icon = leaderboard[t]['avatar']
+            except:
+                username = "NOT IN SERVER"
+                nickname = "NOT IN SERVER"
+                color = "NOT IN SERVER"
+                icon = "NOT IN SERVER"
+            finally:
+                id = leaderboard[t]['id']
+                xp = leaderboard[t]['xp']
+                rank = leaderboard[t]['rank']
+            print("Saving Data for user: ", nickname, "(", xp, ")", sep="")
+            with engine.connect() as con:
+                if len(con.execute(f"SELECT * FROM {table} WHERE ID = '{id}'").fetchall()) > 0:
+                    query = text(f"UPDATE {table} SET Rank = :rank WHERE ID = {id}")
+                    con.execute(query, rank = str(rank))
+                    query = text(f"UPDATE {table} SET Username = :username WHERE ID = {id}")
+                    con.execute(query, username = username)
+                    query = text(f"UPDATE {table} SET Nickname = :nickname WHERE ID = {id}")
+                    con.execute(query, nickname = nickname)
+                    query = text(f"UPDATE {table} SET Color = :color WHERE ID = {id}")
+                    con.execute(query, color = color)
+                    query = text(f"UPDATE {table} SET Icon = :icon WHERE ID = {id}")
+                    con.execute(query, icon = icon)
+                    query = text(f"UPDATE {table} SET {time} = :xp WHERE ID = {id}")
+                    con.execute(query, xp = xp)
+                else:
+                    query = text(f"INSERT INTO {table} (`ID`, `Rank`, `Username`, `Nickname`, `Color`, `Icon`, `{time}`) VALUES (:id, :rank, :username, :nickname, :color, :icon, :xp)")
+                    con.execute(query, id = str(id), rank = str(rank), username = username, nickname = nickname, color = color, icon = icon, xp = str(xp))
+
+record_data(base_url, range(1,6), table, datetime.now().strftime("%B_%d_%Y_%H"))
+
+while True:
+    time = datetime.now()
+    if time >= hour_later:
+        hour_later = time.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+        record_data(base_url, range(1,6), table, datetime.now().strftime("%B_%d_%Y_%H"))
+    else:
+        sleep(5*60)
---
+++ Top Growth.py
@@ -0,0 +1,85 @@
+# Import Libraries
+from sqlalchemy import create_engine, text
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+import json
+
+# Declare Global Vars
+table = 'Gwaff'
+start_date = 'January 14th, 2023 11'
+end_date = ''
+columns = []
+x_val = []
+y_val = []
+annotations = []
+sep = 35
+
+# Define Functions
+def annotate(annotations):
+    heights = [sep]
+    for index, item in enumerate(sorted(annotations, key=lambda x: x[0])):
+        print(item[1], item[0])
+        height = item[0]
+        heights.append(height)
+        if height - heights[len(heights)-2] <= sep:
+            height = heights[len(heights)-1] = heights[len(heights)-2] + sep
+        ax.annotate(item[1], (1.01, height), xycoords=('axes fraction', 'data'), color=item[2], va='center')
+
+# Grab Credentials (you don't think I would be stupid enough to store them within this file)
+with open('credentials.json') as file:
+    credentials = json.load(file)
+
+username = credentials['username']
+password = credentials['password']
+host = credentials['host']
+dbname = credentials['dbname']
+
+# Initialize Items
+engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}/{dbname}')
+plt.style.use('fivethirtyeight')
+fig, ax = plt.subplots()
+ax.set(xlim=(0, 12), xticks=[0,1,2,3,4,5,6,7,8,9,10,11,12],
+       ylim=(0, 1100), yticks=[0,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500],
+       title=("Top Chatters XP Growth"), ylabel=("XP Gained"), xlabel=("Date (M/D/Y) EST"))
+plt.subplots_adjust(left=0.08, right=0.8, bottom=0.08, top=0.9, wspace=0.2, hspace=0.2)
+
+# Parse Date Strings to Make it a Valid Column Name
+start_date = start_date.replace(" ", "_")
+start_date = start_date.replace(",", "")
+start_date = start_date.replace("th", "")
+if end_date == '':
+    end_date = datetime.now().strftime("%B_%d_%Y_%H")
+
+# Create the query
+query = text(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table}' AND ordinal_position BETWEEN (SELECT ordinal_position FROM information_schema.columns WHERE table_name = '{table}' AND column_name = '{start_date}') AND (SELECT ordinal_position FROM information_schema.columns WHERE table_name = '{table}' AND column_name = '{end_date}')")
+
+# Execute the query and fetch the results
+with engine.connect() as con:
+    result = con.execute(query).fetchall()
+    for i in result:
+        columns.append(i[0])
+    xp_columns = len(columns)
+    x_val = np.arange(0, len(columns))
+    columns = str(columns)
+    columns = columns.replace("[", "")
+    columns = columns.replace("]", "")
+    columns = columns.replace("'", "")
+    query = text(f"SELECT Nickname, Color, Icon, {columns} FROM {table} WHERE {start_date} != {end_date} ORDER BY {start_date} - {end_date} LIMIT 15")
+    result = con.execute(query).fetchall()
+for row in result:
+    for i in range(3, xp_columns+3):
+        if i == 3:
+            y = 0
+        else:
+            y = y + row[i] - row[i-1]
+        y_val.append(y)
+    ax.plot(x_val, y_val, linewidth=2.0, color=row[1])
+    y = 0
+    print(row[0], y_val)
+    y_val = []
+    annotations.append(((row[xp_columns+2] - row[3]), row[0], row[1], row[2]))
+
+annotate(annotations)
+
+plt.show()
